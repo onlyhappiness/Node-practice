e03c6945c2430aec08ec32fb1d1a1e8a4bc7ed59
onlyhappiness <onlyhappiness96@gmail.com>
2022-02-24 23:54:29 +0900

    practicing...

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,16 +1,19 @@
-
+const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
   // 에러 퉤
-  throw new Error('testing error')
-  res.status(200).json({ msg: 'products testing route' })
-}
+  // throw new Error('testing error');
+
+  // name이 vase table인 거 찾기
+  const products = await Product.find({ name: 'vase table' });
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' })
-}
+  res.status(200).json({ msg: 'products route' });
+};
 
 module.exports = {
   getAllProducts,
-  getAllProductsStatic
-}
+  getAllProductsStatic,
+};
